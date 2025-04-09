728bd9c73ec65d9d5133fd889d4235fbce428c1d
GGabriel042 <gabrielgonzaga042@gmail.com>
2025-04-09 16:48:39 -0300

    Commit

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -2,19 +2,22 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
 import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
 
 @Log4j2
 
 public class ConnectionFactoryTest01 {
     public static void main(String[] args){
         Producer producer = Producer.builder().name("Studio Deen").build();
-        ProducerRepository.save(producer);
-
-//        log.info("INFO");
-//        log.debug("DEBUG");
-//        log.warn("WARN");
-//        log.error("ERROR");
-//        log.trace("TRACE");
+        Producer producertoUpdate = Producer.builder().id(1).name("MADHOUSE").build();
+//        ProducerService.save(producer);
+//        ProducerService.delete(2);
+//        ProducerService.update(producertoUpdate);
+        List<Producer> producers = ProducerService.findAll();
+//        List<Producer> producers = ProducerService.findByName("Mad");
+        log.info("Producers found '{}'", producers);
     }
 }
