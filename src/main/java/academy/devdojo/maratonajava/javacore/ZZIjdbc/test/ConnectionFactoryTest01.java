fa6699a6026a01ca7ed14d871555e5d381b21691
GGabriel042 <134812062+GGabriel042@users.noreply.github.com>
2025-04-13 15:56:34 -0300

    Add files via upload

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
+
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import academy.devdojo.maratonajava.javacore.ZZIjdbc.service.ProducerService;
+import lombok.extern.log4j.Log4j2;
+
+import java.util.List;
+
+@Log4j2
+
+public class ConnectionFactoryTest01 {
+    public static void main(String[] args){
+        Producer producer = Producer.builder().name("Bones").build();
+        Producer producertoUpdate = Producer.builder().id(1).name("madhouse").build();
+//        ProducerService.save(producer);
+//        ProducerService.delete(2);
+//        ProducerService.update(producertoUpdate);
+//        List<Producer> producers = ProducerService.findAll();
+//        List<Producer> producers = ProducerService.findByName("Mad");
+//        log.info("Producers found '{}'", producers);
+//        ProducerService.showProducerMetaData();
+//        ProducerService.showDriverMetaData();
+//        ProducerService.showTypeScrollWorking();
+//        List<Producer> producers = ProducerService.findByNameAndUpdateToUpperCase("Deen");
+//        List<Producer> producers = ProducerService.findByNameAndInsertWhenNotFound("A-1 pictures");
+//        log.info("Producers not found '{}'", producers);
+//        ProducerService.findByNameAndDelete("A-1 pictures");
+//        List<Producer> producers = ProducerService.findByNamePreparedStatement("Bones");
+//        log.info("Producers found '{}'", producers);
+//        ProducerService.updatePreparedStatement(producertoUpdate);
+        List<Producer> producers = ProducerService.findByNameCallableStatement("Bo");
+        log.info("Producers found '{}'", producers);
+
+    }
+}
